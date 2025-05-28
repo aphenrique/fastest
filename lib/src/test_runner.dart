e8b492a9dea70206a0a6895f5e3305e46e9a17a2
aphenrique <ap_henrique@hotmail.com>
2025-05-28 16:23:27 -0300

    Update version and output

--- lib/src/test_runner.dart
+++ lib/src/test_runner.dart
@@ -49,27 +49,47 @@ class TestRunner {
     );
 
     final failedTests = findFailedTests(result.stdout.toString());
+    final totalTests = _getTotalTests(result.stdout.toString());
+
     if (failedTests.isNotEmpty) {
       print('\nArquivos com falha:');
       for (final test in failedTests) {
         print('❌ $test');
       }
-    } else {
-      print('\n🏆 Todos os testes passaram com sucesso!');
     }
 
     stopwatch.stop();
     final duration = stopwatch.elapsed;
 
-    // Formatando o tempo de execução
     final minutes = duration.inMinutes;
     final seconds = duration.inSeconds % 60;
     final milliseconds = duration.inMilliseconds % 1000;
 
-    print(
-        '\nTempo total de execução: ${_formatDuration(minutes, seconds, milliseconds)}');
+    print('\nResumo da execução:');
+    print('Total de testes: $totalTests');
+    print('Testes com falha: ${failedTests.length}');
+    print('Testes com sucesso: ${totalTests - failedTests.length}');
+    print('Tempo total: ${_formatDuration(minutes, seconds, milliseconds)}');
 
     exit(result.exitCode);
+  }
+
+  int _getTotalTests(String output) {
+    // Procura por uma linha como "00:07 +397 -1: Some tests failed."
+    final resultLine = output.split('\n').firstWhere(
+          (line) =>
+              line.contains('+') && line.contains('-') && line.contains(':'),
+          orElse: () => '',
+        );
+
+    if (resultLine.isEmpty) return 0;
+
+    // Extrai os números após os símbolos + e -
+    final passed = int.tryParse(resultLine.split('+')[1].split(' ')[0]) ?? 0;
+
+    final failed = int.tryParse(resultLine.split('-')[1].split(':')[0]) ?? 0;
+
+    return passed + failed;
   }
 
   String _formatDuration(int minutes, int seconds, int milliseconds) {
